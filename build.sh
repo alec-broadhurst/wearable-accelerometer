f8c0e527a462daff34edaae689d5d8890862196c
alec <alecrbroad@gmail.com>
2026-02-27 14:47:47 -0600

    fixed simlink syntax error

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cmake -S . -B $BUILD_DIR
 
 cmake --build "$BUILD_DIR"
 
-ln -sf "$BUILD_DIR/compile_commands.json ."
+ln -sf "$BUILD_DIR/compile_commands.json" compile_commands.json
 
 echo "Build complete. HEX file in $BUILD_DIR."
