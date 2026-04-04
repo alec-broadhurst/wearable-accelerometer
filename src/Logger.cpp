9baa41b6067ef09088d1dc02e58f09659ec38a3f
alec <alecrbroad@gmail.com>
2026-04-03 22:59:10 -0500

    Added error log file and log methods return a bool for handling after initialization

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -8,12 +8,17 @@ bool Logger::begin(const char* filename) {
     if (!SD.begin()) return false;
 
     _file = SD.open(filename, FILE_WRITE);
+    _error_file = SD.open("error.log", FILE_WRITE);
+    if (!_file || !_error_file) return false;
+
     _file.println("G, Time");
 
     return true;
 }
 
-void Logger::log(const AccelData& data) {
+bool Logger::log(const AccelData& data) {
+    if (!_file) return false;
+
     unsigned long time = millis();
 
     int8_t whole = fixed_whole(data.z);
@@ -25,4 +30,18 @@ void Logger::log(const AccelData& data) {
     _file.print(frac);
     _file.print(",");
     _file.println(time);
+
+    return true;
+}
+
+bool Logger::log_error(const char* msg) {
+    if (!_error_file) return false;
+
+    unsigned long time = millis();
+    _error_file.print("[ERROR] ");
+    _error_file.print(time);
+    _error_file.print(": ");
+    _error_file.println(msg);
+
+    return true;
 }
--- src/Logger.h
+++ src/Logger.h
@@ -8,8 +8,10 @@ class Logger {
     public:
         Logger();
         bool begin(const char* filename);
-        void log(const AccelData& data);
+        bool log(const AccelData& data);
+        bool log_error(const char* msg);
 
     private:
         File _file;
+        File _error_file;
 };
